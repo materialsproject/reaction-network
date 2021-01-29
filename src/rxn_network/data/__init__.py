420f0d0720ac8bd3612c0d7bf13ce219a0890839
Shyam D <shyamd@lbl.gov>
2021-01-28 20:48:04 -0800

    fix data loading

--- src/rxn_network/data/__init__.py
+++ src/rxn_network/data/__init__.py
@@ -1,5 +1,8 @@
+from pathlib import Path
 from monty.serialization import loadfn
 
-G_COMPOUNDS = loadfn("compounds.json")
-G_ELEMS = loadfn("g_els.json")
-G_GASES = loadfn("nist_gas_gf.json")
+cwd = Path(__file__).parent.resolve()
+
+G_COMPOUNDS = loadfn(cwd / "compounds.json")
+G_ELEMS = loadfn(cwd / "g_els.json")
+G_GASES = loadfn(cwd / "nist_gas_gf.json")
