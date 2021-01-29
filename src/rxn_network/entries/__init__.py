af4276a60b2cdb2c86c04a86b7fe7982c9efcc03
Shyam D <shyamd@lbl.gov>
2021-01-29 08:17:57 -0800

    supress import order lint error

--- src/rxn_network/entries/__init__.py
+++ src/rxn_network/entries/__init__.py
@@ -1,6 +1,7 @@
 import hashlib
 from pymatgen.analysis.phase_diagram import PDEntry
 
+
 def _new_pdentry_hash(self):
     data_md5 = hashlib.md5(
         f"{self.composition.formula}_" f"{self.energy}".encode("utf-8")
@@ -11,5 +12,5 @@ def _new_pdentry_hash(self):
 # necessary fix, will be updated in pymatgen in future
 PDEntry.__hash__ = _new_pdentry_hash
 
-from rxn_network.entries.gibbs import GibbsComputedEntry
-from rxn_network.entries.nist import NISTReferenceEntry
+from rxn_network.entries.gibbs import GibbsComputedEntry  # noqa: E402
+from rxn_network.entries.nist import NISTReferenceEntry  # noqa: E402
