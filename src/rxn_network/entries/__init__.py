640c37bacc77e479da9ce099e052d4e13cf32333
Shyam D <shyamd@lbl.gov>
2021-01-28 21:00:44 -0800

    fix init

--- src/rxn_network/entries/__init__.py
+++ src/rxn_network/entries/__init__.py
@@ -11,5 +11,5 @@ def _new_pdentry_hash(self):
 # necessary fix, will be updated in pymatgen in future
 PDEntry.__hash__ = _new_pdentry_hash
 
-from rxn_network.core.entries.gibbs import GibbsComputedEntry
-from rxn_network.core.entries.nist import NISTReferenceEntry
+from rxn_network.entries.gibbs import GibbsComputedEntry
+from rxn_network.entries.nist import NISTReferenceEntry
