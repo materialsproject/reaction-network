e651173f15567210376d209c198622b324c27d41
Shyam D <shyamd@lbl.gov>
2021-02-09 09:49:52 -0800

    more properties for interfaces

--- src/rxn_network/core.py
+++ src/rxn_network/core.py
@@ -27,7 +27,7 @@ class Reaction(MSONable, metaclass=ABCMeta):
 
     @abstractproperty
     def energy(self):
-        " The energy of this reaction in eV per ??"
+        " The energy of this reaction in total eV "
 
     @property
     def compositions(self) -> List[Composition]:
@@ -43,6 +43,25 @@ class Reaction(MSONable, metaclass=ABCMeta):
         """
         return list(set(el for comp in self.compositions for el in comp.elements))
 
+    @property
+    def num_atoms(self) -> float:
+        " Total number of atoms in this reaction "
+        return (
+            sum(
+                [
+                    comp[element] * abs(coeff)
+                    for element in self.elements
+                    for coeff, comp in zip(self.coefficients, self.compositions)
+                ]
+            )
+            / 2
+        )
+
+    @property
+    def energy_per_atom(self) -> float:
+        " The energy per atom of this reaction in eV "
+        return self.energy / self.num_atoms
+
 
 class CostFunction(MSONable, metaclass=ABCMeta):
     " Base definition for a cost function "
@@ -56,16 +75,31 @@ class Enumerator(MSONable, metaclass=ABCMeta):
     " Base definition for a reaction enumeration methodology "
 
     @abstractmethod
+    def estimate_num_reactions(self, entries) -> int:
+        " Estimate of the number of reactions from a list of entires "
+
+    @abstractmethod
     def enumerate(self, entries) -> List[Reaction]:
-        " Evaluates the cost function on a reaction "
+        " Enumerates the potential reactions from the list of entries "
 
 
 class Pathway(MSONable, metaclass=ABCMeta):
     " Base definition for a reaction pathway "
 
+
     @abstractproperty
-    def number_intermediates(self):
-        " The number of intermediates in the total pathway "
+    def reactions(self) -> List[Reaction]:
+
+    @property
+    def energy(self):
+        return sum([rxn.energy for rxn in self.reactions])
+
+    @property
+    def energy_per_atom(self):
+        " Total energy per atom of this reaction pathway"
+        return sum([rxn.energy_per_atom for rxn in self.reactions])
+   
+
 
 
 class ReactionNetwork(MSONable, metaclass=ABCMeta):
