58504349ed02603d9be91368fb140a49feb0cff9
Matthew McDermott <mcdermott@berkeley.edu>
2021-02-11 18:41:06 -0800

    Add first parts of MinimizeGibbsEnumerator

--- src/rxn_network/enumerators/minimize.py
+++ src/rxn_network/enumerators/minimize.py
@@ -9,16 +9,61 @@ from rxn_network.reactions import ComputedReaction
 from rxn_network.enumerators.utils import get_entry_combinations, get_total_chemsys, group_by_chemsys
 
 
-class MinimizePotentialEnumerator(Enumerator):
+class MinimizeGibbsEnumerator(Enumerator):
     def __init__(self):
         pass
-    def enumerate(self):
+    def enumerate(self, entries):
         pass
 
+    @staticmethod
+    def _react_interface(r1, r2, pd, num_entries, grand_pd=None):
+        if grand_pd:
+            interface = InterfacialReactivity(
+                r1,
+                r2,
+                grand_pd,
+                norm=True,
+                include_no_mixing_energy=False,
+                pd_non_grand=pd,
+                use_hull_energy=True
+            )
+        else:
+            interface = InterfacialReactivity(r1,
+                                              r2,
+                                              pd,
+                                              norm=False,
+                                              include_no_mixing_energy=False,
+                                              pd_non_grand=None,
+                                              use_hull_energy=True)
 
-class MinimizeGrandPotentialEnumerator(Enumerator):
-    def __init__(self):
-        pass
+        entries = pd.all_entries
+        rxns = {get_computed_rxn(rxn, entries, num_entries) for _, _, _, rxn,
+                                                                _ in
+                interface.get_kinks()}
+
+        return rxns
+
+    @staticmethod
+    def _get_entry_by_comp(comp, entries):
+        possible_entries = filter(lambda x: x.composition.reduced_composition== comp, entries)
+        return sorted(possible_entries, key=lambda x: x.energy_per_atom)[0]
+
+    @staticmethod
+    def _get_computed_rxn(rxn, entries):
+        reactants = [r.reduced_composition for r in rxn.reactants if not np.isclose(
+            rxn.get_coeff(r), 0)]
+        products = [p.reduced_composition for p in rxn.products if not np.isclose(
+            rxn.get_coeff(p), 0)]
+        reactant_entries = [get_entry_by_comp(r, entries) for r in reactants]
+        product_entries = [get_entry_by_comp(p, entries) for p in products]
+
+        return ComputedReaction(reactant_entries, product_entries)
+
+
+class MinimizeOpenGibbsEnumerator(MinimizeGibbsEnumerator):
+    def __init__(self, open_entries):
+        super().__init__()
+        self.open_entries = open_entries
     def enumerate(self):
         pass
 
--- src/rxn_network/enumerators/utils.py
+++ src/rxn_network/enumerators/utils.py
@@ -1,4 +1,5 @@
 from itertools import permutations
+from pymatgen.entries.computed_entries import ComputedEntry
 from rxn_network.utils import limited_powerset
 from rxn_network.reactions.computed import ComputedReaction
 
@@ -9,7 +10,6 @@ def get_total_chemsys(entries):
 
 def group_by_chemsys(combos):
     combo_dict = {}
-
     for combo in combos:
         key = get_total_chemsys(combo)
         if key in combo_dict:
@@ -18,3 +18,16 @@ def group_by_chemsys(combos):
             combo_dict[key] = [combo]
 
     return combo_dict
+
+def stabilize_entries(pd, entries_to_adjust, tol=1e-6):
+    indices = [pd.all_entries.index(entry) for entry in entries_to_adjust]
+    new_entries = []
+    for idx, entry in zip(indices, entries_to_adjust):
+        e_above_hull = pd.get_e_above_hull(entry)
+        entry_dict = entry.to_dict()
+        entry_dict["energy"] = entry.uncorrected_energy + \
+                               (e_above_hull * entry.composition.num_atoms)
+        new_entry = ComputedEntry.from_dict(entry_dict)
+        new_entries.append(new_entry)
+    return new_entries
+
