9cd1ad36af40f92dda2c20c801ad5e165904514f
Matthew McDermott <mcdermott@berkeley.edu>
2022-03-08 23:57:58 -0800

    update init

--- src/rxn_network/enumerators/__init__.py
+++ src/rxn_network/enumerators/__init__.py
@@ -2,16 +2,8 @@
 Reaction enumerator classes and associated utilities.
 """
 
-import ray
-
 from rxn_network.enumerators.basic import BasicEnumerator, BasicOpenEnumerator
 from rxn_network.enumerators.minimize import (
     MinimizeGibbsEnumerator,
     MinimizeGrandPotentialEnumerator,
 )
-
-if not ray.is_initialized():
-    ray.init(
-        _redis_password="test_password",
-        ignore_reinit_error=True,
-    )
