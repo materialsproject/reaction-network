841fbb529de258958d302838fe1a999f6c63e255
Matthew McDermott <mcdermott@berkeley.edu>
2022-03-28 15:17:20 -0700

    rewrite retrosynthesis builder with new models

--- src/rxn_network/builders/retrosynthesis.py
+++ src/rxn_network/builders/retrosynthesis.py
@@ -1,10 +1,19 @@
+""" Builder(s) for generating synthesis recipe documents."""
+from math import ceil
+from typing import Dict, List, Optional, Any
+
 from maggma.builders import Builder
 from maggma.core import Store
 from maggma.utils import grouper
+from monty.json import jsanitize
 from pymatgen.core.composition import Composition
 
 from rxn_network.core.cost_function import CostFunction
 from rxn_network.reactions.reaction_set import ReactionSet
+from rxn_network.utils.models import (
+    ComputedSynthesisRecipe,
+    ComputedSynthesisRecipesDoc,
+)
 
 
 class SynthesisRecipeBuilder(Builder):
@@ -13,80 +22,153 @@ class SynthesisRecipeBuilder(Builder):
     """
 
     def __init__(
-        self, tasks: Store, tasks_fs: Store, recipes: Store, cf: CostFunction, **kwargs
+        self,
+        tasks: Store,
+        recipes: Store,
+        cf: CostFunction,
+        tasks_fs: Optional[Store] = None,
+        query: Optional[Dict] = None,
+        **kwargs,
     ):
         self.tasks = tasks
         self.tasks_fs = tasks_fs
         self.recipes = recipes
         self.cf = cf
+        self.query = query
+        self.kwargs = kwargs
 
-        super().__init__(source=tasks, target=recipes, **kwargs)
-        self.sources.append(self.tasks_fs)
+        sources = [tasks]
+
+        if tasks_fs:
+            sources.append(tasks_fs)
+
+        super().__init__(sources=sources, targets=[recipes], **kwargs)
+
+    def ensure_indexes(self):
+        """
+        Ensures indexes for the tasks, (tasks_fs), and recipes collections.
+        """
+        self.tasks.ensure_index(self.tasks.key)
+        self.tasks.ensure_index(self.tasks.last_updated_field)
+        self.recipes.ensure_index(self.recipes.key)
+        self.recipes.ensure_index(self.recipes.last_updated_field)
+
+        if self.tasks_fs:
+            self.tasks_fs.ensure_index(self.tasks_fs.key)
+
+    def prechunk(self, number_splits: int):
+        """
+        Prechunk method to perform chunking by the key field.
+        """
+        keys = self._find_to_process()
+
+        N = ceil(len(keys) / number_splits)
+
+        for split in grouper(keys, N):
+            yield {"query": {self.tasks.key: {"$in": list(split)}}}
 
     def get_items(self):
         """Get the items to process."""
-        self.logger.info("Starting {} Builder".format(self.__class__.__name__))
+        to_process_task_ids = self._find_to_process()
+
+        self.logger.info(
+            "Processing {} task docs for synthesis recipes".format(
+                len(to_process_task_ids)
+            )
+        )
+        self.total = len(to_process_task_ids)
+
+        for task_id in to_process_task_ids:
+            task = self.tasks.query_one({"task_id": task_id})
+            if self.tasks_fs:
+                rxns = self.tasks_fs.query_one(
+                    {"task_id": task_id},
+                )["rxns"]
+                task["rxns"] = rxns
+                if not rxns:
+                    self.logger.warning(
+                        "Missing rxns from GridFSStore for task_id {}".format(task_id)
+                    )
+            else:
+                if not task.get("rxns"):
+                    self.logger.warning("Missing rxns in task {}".format(task_id))
+
+            if task is not None:
+                yield task
+            else:
+                pass
+
+    def process_item(self, item):
+        """Creates a synthesis recipe document from the task document."""
+        rxns_dict = item["rxns"]
+        targets = item["targets"]
+        elements = item["elements"]
+        chemsys = item["chemsys"]
+        added_elements = item["added_elements"]
+        added_chemsys = item["added_chemsys"]
+        enumerators = item["enumerators"]
+        mu_func = None  # TODO: incorporate this?
+
+        if len(targets) > 1:
+            self.logger.warning(
+                "Enumerator has multiple targets for task_id {}".format(item["task_id"])
+            )
+            self.logger.warning("Selecting first target...")
+
+        target = item["targets"][0]
+        target_comp = Composition(target)
+
+        self.logger.debug("Creating synthesis recipes for {}".format(item["task_id"]))
+
+        rxns = ReactionSet.from_dict(rxns_dict).get_rxns()
+        costs = [self.cf.evaluate(rxn) for rxn in rxns]
+        recipes = [
+            ComputedSynthesisRecipe.from_computed_rxn(
+                rxn, cost=cost, target=target_comp, mu_func=mu_func
+            )
+            for rxn, cost in zip(rxns, costs)
+        ]
+
+        d: Dict[str, Any] = {}
+
+        d["recipes"] = recipes
+        d["target_composition"] = target_comp
+        d["target_formula"] = target
+        d["elements"] = elements
+        d["chemsys"] = chemsys
+        d["added_elements"] = added_elements
+        d["added_chemsys"] = added_chemsys
+        d["enumerators"] = enumerators
+        d["cost_function"] = self.cf
+
+        doc = ComputedSynthesisRecipesDoc(**d)
+
+        return jsanitize(
+            doc.dict(),
+            strict=True,
+            allow_bson=True,
+        )
+
+    def update_targets(self, items):
+        """
+        Inserts the new synthesis recipe docs into the Synthesis Recipes collection
+        """
+        docs = list(filter(None, items))
+
+        if len(docs) > 0:
+            self.logger.info(f"Found {len(docs)} synthesis recipe docs to update")
+            self.recipes.update(docs)
+        else:
+            self.logger.info("No items to update")
+
+    def _find_to_process(self):
+        self.logger.info("Synthesis Recipe builder started.")
+
+        self.logger.info("Setting up indexes.")
         self.ensure_indexes()
 
-        keys = self.target.newer_in(self.source, criteria=self.query, exhaustive=True)
-        if self.retry_failed:
-            if isinstance(self.query, (dict)):
-                failed_query = {"$and": [self.query, {"state": "failed"}]}
-            else:
-                failed_query = {"state": "failed"}
-            failed_keys = self.target.distinct(self.target.key, criteria=failed_query)
-            keys = list(set(keys + failed_keys))
+        task_keys = set(self.tasks.distinct("task_id", criteria=self.query))
+        print(task_keys)
+        updated_tasks = set(self.recipes.newer_in(self.tasks))
 
-        self.logger.info("Processing {} items".format(len(keys)))
-
-        if self.projection:
-            projection = list(
-                set(self.projection + [self.source.key, self.source.last_updated_field])
-            )
-        else:
-            projection = None
-
-        self.total = len(keys)
-        for chunked_keys in grouper(keys, self.chunk_size):
-            chunked_keys = list(chunked_keys)
-            for key in chunked_keys:
-                query = self.tasks.query_one(
-                    criteria={self.tasks.key: key},
-                    properties=projection,
-                )
-                query_fs = self.tasks_fs.query_one(
-                    criteria={"task_id": key},
-                    properties=projection,
-                )
-
-                if query_fs is not None:
-                    del query_fs["task_id"]
-                    query["rxns"] = query_fs
-
-                yield query
-
-    def unary_function(self, item):
-        """Map function to process a single item."""
-        try:
-            rxns_dict = item["rxns"]
-        except KeyError:
-            self.logger.info("No rxns found for task {}!".format(item["task_id"]))
-            return
-
-        target = Composition(item["targets"][0])
-        added_elems = item["added_elems"]
-        enumerators = item["enumerators"]
-
-        rxn_set = ReactionSet.from_dict(rxns_dict)
-        df = rxn_set.to_dataframe(cost_function=self.cf, target=target)
-        df["rxn"] = df["rxn"].astype("str")
-
-        d = {}
-        d["recipes"] = df.to_dict(orient="records")
-        d["target"] = target
-        d["target_formula"] = target.reduced_formula
-        d["added_elems"] = added_elems
-        d["enumerators"] = enumerators
-        d["cost_function"] = self.cf.as_dict()
-
-        return d
+        return updated_tasks & task_keys
--- src/rxn_network/utils/models.py
+++ src/rxn_network/utils/models.py
@@ -1,12 +1,13 @@
 """Core definition for various task and synthesis recipe documents"""
-from enum import Enum
-from typing import Any, List, Optional
 from datetime import datetime
+from typing import List, Optional, Any, Dict
 
 from pydantic import BaseModel, Field  # pylint: disable=no-name-in-module
 from pymatgen.core.composition import Composition, Element
+from pymatgen.entries.computed_entries import ComputedEntry
 
 from rxn_network.core import CostFunction, Enumerator, Network
+from rxn_network.reactions.computed import ComputedReaction
 from rxn_network.reactions.reaction_set import ReactionSet
 
 
@@ -21,6 +22,9 @@ class EnumeratorTask(BaseModel):
     dir_name: str = Field(None, description="The directory name")
     rxns: Optional[ReactionSet] = Field(
         None, description="The reaction set. Optional to allow for storing in GridFS."
+    )
+    targets: List[str] = Field(
+        None, description="The target formulas used in the enumerator(s)."
     )
     elements: List[Element] = Field(
         None, description="The elements of the total chemical system"
@@ -122,6 +126,30 @@ class Phase(BaseModel):
     )
     icsd_ids: List[int] = Field(None, description="A list of ICSD ids for the phase.")
 
+    @classmethod
+    def from_computed_entry(cls, entry: ComputedEntry, **kwargs) -> "Phase":
+        """
+        Simple constructor for a phase from a pymatgen ComputedEntry object.
+        """
+        data: Dict[str, Any] = {}
+
+        data["name"] = entry.name
+        data["composition"] = entry.composition
+        data["energy"] = entry.energy
+        data["energy_per_atom"] = entry.energy_per_atom
+        data["reduced_formula"] = entry.composition.reduced_formula
+        data["temperature"] = getattr(entry, "temperature", None)
+        data["entry_id"] = entry.entry_id
+        data["e_above_hull"] = entry.data.get("e_above_hull", None)
+        data["is_metastable"] = (
+            data["e_above_hull"] > 0 if data["e_above_hull"] else None
+        )
+        data["is_experimental"] = entry.is_experimental
+        data["icsd_ids"] = entry.data.get("icsd_ids", None)
+        d = {k: v for k, v in data.items() if v is not None}
+
+        return cls(**d, **kwargs)
+
 
 class ReactionThermo(BaseModel):
     """
@@ -140,8 +168,10 @@ class ReactionSelectivity(BaseModel):
     potential distance)
     """
 
-    chempot_distance: float = Field(None, description="Chemical potential distance")
-    c_score: float = Field(None, description="Competitiveness Score")
+    chempot_distance: float = Field(
+        None, description="Chemical potential distance score of the reaction"
+    )
+    c_score: float = Field(None, description="Competition score of the reaction")
     mu_func: str = Field(
         None, description="Name of function used to calculate chempot_distance metric"
     )
@@ -170,6 +200,32 @@ class Reaction(BaseModel):
         None, description="List of elements in the reaction"
     )
     chemsys: str = Field(None, description="The chemical system string (e.g., Fe-Li-O)")
+    compositions: List[Composition] = Field(
+        None, description="List of composition objects in the reaction equation."
+    )
+    coefficients: List[float] = Field(
+        None,
+        description="List of coefficients corresponding to compositions in reactin equation.",
+    )
+
+    @classmethod
+    def from_computed_rxn(cls, rxn: ComputedReaction, **kwargs) -> "Reaction":
+        """
+        Simple constructor for a Reaction model from a ComputedReaction object.
+        """
+        data: Dict[str, Any] = {}
+
+        data["rxn_str"] = str(rxn)
+        data["reactants"] = [Phase.from_computed_entry(r) for r in rxn.reactant_entries]
+        data["products"] = [Phase.from_computed_entry(p) for p in rxn.product_entries]
+        data["elements"] = rxn.elements
+        data["chemsys"] = rxn.chemical_system
+        data["compositions"] = rxn.compositions
+        data["coefficients"] = rxn.coefficients.tolist()
+
+        d = {k: v for k, v in data.items() if v is not None}
+
+        return cls(**d, **kwargs)
 
 
 class ComputedSynthesisRecipe(BaseModel):
@@ -186,25 +242,69 @@ class ComputedSynthesisRecipe(BaseModel):
     cost: float = Field(None, description="Calculated cost of the reaction")
     byproducts: List[str] = Field(None, description="List of byproduct formulas")
 
+    @classmethod
+    def from_computed_rxn(
+        cls,
+        rxn: ComputedReaction,
+        cost: float,
+        target: Composition,
+        mu_func: Optional[str] = None,
+        **kwargs
+    ) -> "ComputedSynthesisRecipe":
+        """
+        Simple constructor for a Reaction model from a ComputedReaction object.
+        """
+        data: Dict[str, Any] = {}
+
+        data["rxn"] = Reaction.from_computed_rxn(rxn)
+        data["thermo"] = ReactionThermo(
+            energy_per_atom=rxn.energy_per_atom,
+            energy_uncertainty_per_atom=rxn.energy_uncertainty_per_atom,
+        )
+        data["selectivity"] = ReactionSelectivity(
+            chempot_distance=rxn.data.get("chempot_distance"),
+            c_score=rxn.data.get("c_score"),
+            mu_func=mu_func,
+        )
+        data["heuristics"] = ReactionHeuristics(
+            is_separable=rxn.is_separable(target),
+            fraction_experimental=sum([e.is_experimental for e in rxn.entries])
+            / len(rxn.entries),
+        )
+        data["cost"] = cost
+        data["byproducts"] = sorted(
+            [c.reduced_formula for c in set(rxn.products) - set(target)]
+        )
+
+        d = {k: v for k, v in data.items() if v is not None}
+
+        return cls(**d, **kwargs)
+
 
 class ComputedSynthesisRecipesDoc(BaseModel):
     """
     Model for a document containing computed synthesis recipes for one material
     """
 
-    rxns: List[ComputedSynthesisRecipe] = Field(
+    task_id: str = Field(None, description="Task ID")
+    recipes: List[ComputedSynthesisRecipe] = Field(
         None, description="List of computed synthesis recipes"
     )
     target_composition: Composition = Field(None, description="Target composition")
     target_formula: str = Field(None, description="The target formula")
-    added_elems: List[Element] = Field(None, description="List of elements added")
+    elements: List[Element] = Field(None, description="List of elements in the recipe")
     chemsys: str = Field(
         None, description="The total chemical system string (e.g., Fe-Li-O)."
+    )
+    added_elements: List[Element] = Field(None, description="List of elements added")
+    added_chemsys: str = Field(
+        None,
+        description="The chemical system string of the added elements (e.g., Fe-Li-O)",
     )
     enumerators: List[Enumerator] = Field(
         None,
         description="A list of the enumerator objects used to calculate the reactions.",
     )
-    cost_function: str = Field(
+    cost_function: CostFunction = Field(
         None, description="The cost function used to calculate the cost."
     )
